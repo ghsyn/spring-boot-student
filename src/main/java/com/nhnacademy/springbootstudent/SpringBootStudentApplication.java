63f73becb77446e02453bc8ebacefa9beab2aa17
김시연 <swanshawn715@gmail.com>
2023-05-31 16:47:00 +0900

    banner 변경

--- src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
+++ src/main/java/com/nhnacademy/springbootstudent/SpringBootStudentApplication.java
@@ -1,5 +1,6 @@
 package com.nhnacademy.springbootstudent;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,10 @@ public class SpringBootStudentApplication {
 	public static void main(String[] args) {
 		// 객체 생성해서 사용해도 되고, 빌더로도 가능
 		SpringApplication.run(SpringBootStudentApplication.class, args);
+
+//		SpringApplication application = new SpringApplication(SpringBootStudentApplication.class);
+//		application.setBannerMode(Banner.Mode.OFF);
+//		application.run(args);
 	}
 
 }
