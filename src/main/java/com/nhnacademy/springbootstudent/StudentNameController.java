3941264d0b987467a3fb5c6c8ce497080c3b1e79
김시연 <swanshawn715@gmail.com>
2023-05-31 18:21:16 +0900

    외부화 설정 완료

--- src/main/java/com/nhnacademy/springbootstudent/StudentNameController.java
+++ src/main/java/com/nhnacademy/springbootstudent/StudentNameController.java
@@ -1,11 +1,26 @@
 package com.nhnacademy.springbootstudent;
 
+import com.nhnacademy.springbootstudent.configuration.StudentProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class StudentNameController {
     @Value("${nhn.student.name}")
     private String name;
 
+    @Autowired
+    private StudentProperties studentProperties;
 
+    @GetMapping("/student-name")
+    public String getStudentName() {
+        return name;
+    }
+
+    @GetMapping("/student-name2")
+    public String getStudentName2() {
+        return studentProperties.getName();
+    }
 }
